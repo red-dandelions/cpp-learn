2eb88c1ae2c03ff7248fd83120c77146ec350403
red-dandelions <1011694931@qq.com>
2022-11-28 23:52:50 +0800

    fix and add simd test

--- templates/test/simd_sum_test.cc
+++ templates/test/simd_sum_test.cc
@@ -1,19 +1,27 @@
 #include "templates/simd_sum.h"
 
 #include "gtest/gtest.h"
+#include <iostream>
 #include <vector>
 
-TEST(SIMDSum, sum) {
-  const int Size = 100000;
-  std::vector<int> a(Size, 1), b(Size, 2), c(Size, 3);
-  templates::Sum(a, b);
-  for (size_t i = 0; i < Size; ++i) {
-    ASSERT_EQ(a[i], c[i]);
+TEST(SIMDSum, normal) {
+  const int Size = 10;
+  {
+    std::vector<int> a(Size, 1), b(Size, 2), c(Size, 3);
+    templates::Sum(a, b);
+    for (size_t i = 0; i < Size; ++i) {
+      ASSERT_EQ(a[i], c[i]);
+    }
   }
+}
 
-  std::vector<float> a(Size, 1.5), b(Size, 2.5), c(Size, 4);
-  templates::Sum(a, b);
-  for (size_t i = 0; i < Size; ++i) {
-    ASSERT_EQ(a[i], c[i]);
+TEST(SIMDSum, simd) {
+  const int Size = 10;
+  {
+    std::vector<float> a(Size, 1.5), b(Size, 2.5), c(Size, 4);
+    templates::Sum(a, b);
+    for (size_t i = 0; i < Size; ++i) {
+      ASSERT_EQ(a[i], c[i]);
+    }
   }
 }
