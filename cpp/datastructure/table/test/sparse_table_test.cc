c667ea5afc71c94f212c24ffe45f47ed6559d658
moshouhua <moshouhua@bigo.sg>
2022-12-26 16:45:15 +0800

    format and add pair_tree

--- cpp/datastructure/table/test/sparse_table_test.cc
+++ cpp/datastructure/table/test/sparse_table_test.cc
@@ -2,11 +2,13 @@
  * 给定数组 input 和 q，数组 q 每次询问input区间 [q[i][0], q[i][1]] 内数字的最大值。
  */
 #include "cpp/datastructure/table/sparse_table.h"
+
 #include "gtest/gtest.h"
 
 TEST(SparseTableTest, solve) {
   std::vector<int> input = {9, 3, 1, 7, 5, 6, 0, 8};
-  std::vector<std::vector<int>> q = {{1, 6},{1,5},{2,7},{2,6},{1,8},{4,8},{3,7},{1,8}};
+  std::vector<std::vector<int>> q = {{1, 6}, {1, 5}, {2, 7}, {2, 6},
+                                     {1, 8}, {4, 8}, {3, 7}, {1, 8}};
   std::vector<int> output = {9, 9, 7, 7, 9, 8, 7, 9};
 
   table::SparseTable<int> st(input);
